8a1fca77b9265436a567037369778583771c4bd1
jagadeesh-431 <jagadeeshtmr43@gmail.com>
2025-03-19 19:41:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var fannn = document.getElementById("fannn")
+function fan_on() {
+    fannn.className = "fan"; // Start rotation
+}
+
+function fan_off() {
+    fannn.className = "off"; // Stop rotation
+}
